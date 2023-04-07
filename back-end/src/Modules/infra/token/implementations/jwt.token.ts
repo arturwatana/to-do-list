7a5528fe3c2f231be601f2fe2053059c1ab47af5
Artur Watanabe <arturwatanabe@gmail.com>
2023-04-07 17:40:04 -0300

    fix: increase token expiration

--- back-end/src/Modules/infra/token/implementations/jwt.token.ts
+++ back-end/src/Modules/infra/token/implementations/jwt.token.ts
@@ -16,7 +16,7 @@ export class JWTToken implements IToken {
       this.TOKEN_SECRET_CRYPTO,
       {
         subject: id,
-        expiresIn: "20m",
+        expiresIn: "8h",
       }
     );
     return token;
